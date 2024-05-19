4eed888aea37bc7e91020ac6d6b7a5e40f54091a
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2024-05-19 09:49:18 +0700

    setup rule + cf

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,14 +30,129 @@ class MainController extends Controller
             'keyakinan_metode_panen' => $request->input('keyakinan_metode_panen'),
         ];
 
-        PakarTernak::create($data);
+        // PakarTernak::create($data);
 
         // // Add response for empty fields
         if (empty($data['waktu_panen']) || empty($data['budget_tahunan']) || empty($data['metode_panen']) || empty($data['jenis_pakan']) || empty($data['jenis_ikan']) || empty($data['keyakinan_metode_pakan']) || empty($data['keyakinan_metode_panen'])) {
             return response()->json(['message' => 'Field wajib diisi'], 400);
         }
 
+        $urgensi = "";
+        $strategi = "";
+        $rekomendasi = "";
+        $cfurgensi = 1;
+        $cfstrategi = 1;
+        $cfrekomendasi = 1;
+
+        #rule 1-4
+        if($data['waktu_panen'] == "waktu" && $data['metode_panen'] == "perlu_drainase") {
+            $urgensi = "sedikit";
+            $cfrule = 1;
+            $cfurgensi = min($data['keyakinan_metode_panen'], 1) * $cfrule;
+        }
+
+        if($data['waktu_panen'] == "waktu" && $data['metode_panen'] == "tidak_perlu_drainase") {
+            $urgensi = "tidak perlu";
+            $cfrule = 1;
+            $cfurgensi = min($data['keyakinan_metode_panen'], 1) * $cfrule;
+        }
+
+        if($data['waktu_panen'] == "berat" && $data['metode_panen'] == "perlu_drainase") {
+            $urgensi = "perlu";
+            $cfrule = 1;
+            $cfurgensi = min($data['keyakinan_metode_panen'], 1) * $cfrule;
+        }
+
+        if($data['waktu_panen'] == "berat" && $data['metode_panen'] == "tidak_perlu_drainase") {
+            $urgensi = "sedikit";
+            $cfrule = 1;
+            $cfurgensi = min($data['keyakinan_metode_panen'], 1) * $cfrule;
+        }
+
+        #rule 5-7
+        if($data['jenis_pakan'] == "bebas" && $data['jenis_ikan'] == "sama_jenis") {
+            $strategi = "umum";
+            $cfrule = 1;
+            $cfstrategi = min($data['keyakinan_metode_pakan'], 1) * $cfrule;
+        }
+
+        if($data['jenis_pakan'] == "bebas" && $data['jenis_ikan'] == "campur_jenis") {
+            $strategi = "spesifik";
+            $cfrule = 0.8;
+            $cfstrategi = min($data['keyakinan_metode_pakan'], 1) * $cfrule;
+        }
+
+        if($data['jenis_pakan'] == "spesifik") {
+            $strategi = "spesifik";
+            $cfrule = 1;
+            $cfstrategi = min($data['keyakinan_metode_pakan'], 1) * $cfrule;
+        }
+
+        #rule 8 - 11
+        if($urgensi == "tidak perlu" && $strategi == "umum") {
+            $rekomendasi = "tidak perlu alat";
+            $cfrule = 0.8;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "tidak perlu" && $strategi == "spesifik") {
+            $rekomendasi = "alat pemberi makan otomatis";
+            $cfrule = 0.85;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "sedikit" && $strategi == "umum") {
+            $rekomendasi = "sistem pompa air";
+            $cfrule = 0.85;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "sedikit" && $strategi == "spesifik") {
+            $rekomendasi = "paket 1";
+            $cfrule = 0.9;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        #rule 12 - 17
+        if($urgensi == "perlu" && $strategi == "umum" && $data['budget_tahunan'] == "rendah") {
+            $rekomendasi = "sistem pompa air";
+            $cfrule = 0.9;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+        
+        if($urgensi == "perlu" && $strategi == "umum" && $data['budget_tahunan'] == "sedang") {
+            $rekomendasi = "paket 2";
+            $cfrule = 0.85;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "perlu" && $strategi == "umum" && $data['budget_tahunan'] == "tinggi") {
+            $rekomendasi = "paket 3";
+            $cfrule = 0.9;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "perlu" && $strategi == "spesifik" && $data['budget_tahunan'] == "rendah") {
+            $rekomendasi = "paket 1";
+            $cfrule = 0.8;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "perlu" && $strategi == "spesifik" && $data['budget_tahunan'] == "sedang") {
+            $rekomendasi = "paket 4";
+            $cfrule = 0.85;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        if($urgensi == "perlu" && $strategi == "spesifik" && $data['budget_tahunan'] == "tinggi") {
+            $rekomendasi = "paket 5";
+            $cfrule = 0.95;
+            $cfrekomendasi = min($cfurgensi, $cfstrategi, 1) * $cfrule;
+        }
+
+        $result = ["keyakinan" => $cfrekomendasi*100, "rekomendasi" => $rekomendasi];
+
         // Return data in JSON format
-        return response()->json($data);
+        return response()->json($result);
     }
 }
